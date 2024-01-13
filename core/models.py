f4755cfc04c9cc65774304e0f821793cf383371d
sahar <you@example.com>
2024-01-13 15:32:16 +0330

    feat:add image and comment models in core app

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    content = models.TextField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    user = models.ForeignKey("customers.CustomUser", on_delete=models.CASCADE, blank=True, null=True)
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
