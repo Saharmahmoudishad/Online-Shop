152909f318556986e87ca3fcefb668832b9b6d64
sahar <you@example.com>
2024-01-14 20:52:06 +0330

    feat: add CategoryProduct models in product app 1. in this model using TreeForeignKey 2. saving image of categories are handled in Image model 3. get_absolute_url and image_tag methods were made in this class 4. this model was added in panel admin

--- product/admin.py
+++ product/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 
-from product.models import Color, Size
+from product.models import Color, Size, CategoryProduct
 
 
-# Register your models here.
+class CategoryProductAdmin(admin.ModelAdmin):
+    list_display = ["title", "parent", "status", "image", ]
+
+
 class SizeAdmin(admin.ModelAdmin):
     list_display = ["name", "code", ]
 
@@ -12,5 +15,6 @@ class ColorAdmin(admin.ModelAdmin):
     list_display = ["name", "code", "color_tag", ]
 
 
+admin.site.register(CategoryProduct, CategoryProductAdmin)
 admin.site.register(Size, SizeAdmin)
 admin.site.register(Color, ColorAdmin)
--- product/models.py
+++ product/models.py
@@ -1,6 +1,10 @@
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
-
+from django.urls import reverse
 from django.utils.safestring import mark_safe
+from mptt.models import MPTTModel, TreeForeignKey
+
+from core.models import Image
 
 
 class Size(models.Model):
@@ -29,3 +33,42 @@ class Color(models.Model):
         if self.code is not None:
             return mark_safe('<p style ="background-color:{}">Color</p>'.format(self.code))
         return ""
+
+
+class CategoryProduct(MPTTModel):
+    STATUS = (('True', 'True'), ('False', 'False'))
+    title = models.CharField(max_length=300)
+    slug = models.SlugField(null=False, unique=True)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    status = models.CharField(max_length=15, choices=STATUS)
+    image = models.ImageField(blank=True, upload_to='images/')
+    keywords = models.CharField(max_length=255)
+    description = models.TextField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class MPTTMeta:
+        order_insertion_by = ['title']
+
+    class Meta:
+        verbose_name_plural = "Products' Categories"
+
+    def get_absolute_url(self):
+        return reverse('category_detail', kwargs={'slug': self.slug})
+
+    def __str__(self):
+        if self.parent:
+            full_path = [self.title]
+            k = self.parent
+            while k is not None:
+                full_path.append(k.title)
+                k = k.parent
+            return ' / '.join(full_path[::-1])
+        else:
+            return self.title
+
+    def image_tag(self):
+        images = Image.objects.filter(content_type=ContentType.objects.get_for_model(self), object_id=self.id)
+        if images.exists():
+            return mark_safe('<img src="{}" height="50"/>'.format(images.first().image.url))
+        return None
