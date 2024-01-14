27dc9e607a9825f7e1690bff990674326cdd81b0
sahar <you@example.com>
2024-01-14 23:57:27 +0330

    feat: add model Atribute, product and variants for products app In addition, adding urls file for product app

--- product/admin.py
+++ product/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from product.models import Color, Size, CategoryProduct
+from product.models import Color, Size, CategoryProduct, Brand, Material, Attribute, Products
 
 
 class CategoryProductAdmin(admin.ModelAdmin):
@@ -19,6 +19,26 @@ class ColorAdmin(admin.ModelAdmin):
     list_display = ["name", "code", "color_tag", ]
 
 
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ["name", ]
+
+
+class AttributeAdmin(admin.ModelAdmin):
+    list_display = ["attribute", "type", ]
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["title", "category", "status", "image_tag", ]
+
+
+class VariantsAdmin(admin.ModelAdmin):
+    list_display = ["title", "color", "size", "brand", "material", "attribute", "price", "quantity", "image_tag", ]
+
+
 admin.site.register(CategoryProduct, CategoryProductAdmin)
+admin.site.register(Brand, BrandAdmin)
 admin.site.register(Size, SizeAdmin)
 admin.site.register(Color, ColorAdmin)
+admin.site.register(Material, MaterialAdmin)
+admin.site.register(Attribute, AttributeAdmin)
+admin.site.register(Products, ProductAdmin)
--- product/models.py
+++ product/models.py
@@ -3,8 +3,10 @@ from django.db import models
 from django.urls import reverse
 from django.utils.safestring import mark_safe
 from mptt.models import MPTTModel, TreeForeignKey
+from ckeditor_uploader.fields import RichTextUploadingField
 
 from core.models import Image
+from customers.models import CustomUser
 
 
 class Brand(models.Model):
@@ -12,7 +14,7 @@ class Brand(models.Model):
     slug = models.SlugField(max_length=300)
 
     def get_absolute_url(self):
-        return reverse('catalog:brand', args=[self.slug])
+        return reverse('product:brand', args=[self.slug])
 
     def __str__(self):
         return self.title
@@ -52,6 +54,14 @@ class Color(models.Model):
         return ""
 
 
+class Attribute(models.Model):
+    attribute = models.CharField(max_length=300)
+    type = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f'{self.attribute}_{self.type}'
+
+
 class CategoryProduct(MPTTModel):
     STATUS = (('True', 'True'), ('False', 'False'))
     title = models.CharField(max_length=300)
@@ -89,3 +99,62 @@ class CategoryProduct(MPTTModel):
         if images.exists():
             return mark_safe('<img src="{}" height="50"/>'.format(images.first().image.url))
         return None
+
+
+class Products(models.Model):
+    STATUS = (('True', 'True'), ('False', 'False'))
+    VARIANTS = (('None', 'None'), ('Size', 'Size'), ('Color', 'Color'), ('Size-Color', 'Size-Color'),
+                ('Brand-Size-Color', 'Brand-Size-Color'))
+    category = models.ForeignKey(CategoryProduct, on_delete=models.CASCADE, related_name="products")
+    title = models.CharField(max_length=150)
+    keywords = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.PositiveIntegerField(default=0)
+    quantity = models.PositiveIntegerField(default=1)
+    variant = models.CharField(max_length=30, choices=VARIANTS, default='None')
+    detail = RichTextUploadingField()
+    slug = models.SlugField(null=False, unique=True)
+    status = models.CharField(max_length=15, choices=STATUS)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, related_name="products")
+
+    # like = models.ManyToManyField(CustomUser, through='Like', related_name='liked_item')
+    # tags = TaggableManager()
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Products"
+
+    def get_absolute_url(self):
+        return reverse('category_detail', kwargs={'slug': self.slug})
+
+    def image_tag(self):
+        images = Image.objects.filter(content_type=ContentType.objects.get_for_model(self), object_id=self.id)
+        if images.exists():
+            return mark_safe('<img src="{}" height="50"/>'.format(images.first().image.url))
+        return None
+
+
+class Variants(models.Model):
+    title = models.CharField(max_length=100, blank=True, null=True)
+    product = models.ForeignKey(Products, on_delete=models.CASCADE, related_name="products")
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name="products", null=True)
+    size = models.ForeignKey(Size, on_delete=models.SET_NULL, related_name="products", null=True)
+    color = models.ForeignKey(Color, on_delete=models.CASCADE, related_name="products", null=True)
+    material = models.ForeignKey(Material, on_delete=models.CASCADE, related_name="products", null=True)
+    attribute = models.ForeignKey(Attribute, on_delete=models.CASCADE, related_name="products",  null=True)
+    price = models.PositiveIntegerField(default=0)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.title
+
+    def image_tag(self):
+        images = Image.objects.filter(content_type=ContentType.objects.get_for_model(self), object_id=self.id)
+        if images.exists():
+            return mark_safe('<img src="{}" height="50"/>'.format(images.first().image.url))
+        return None
+
+
