c88f82f4bf116ccac477ef64a100273121e0d140
sahar <you@example.com>
2024-01-14 19:52:40 +0330

    feat: add size , and color models in products app add these model in admin

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from product.models import Color, Size
+
+
 # Register your models here.
+class SizeAdmin(admin.ModelAdmin):
+    list_display = ["name", "code", ]
+
+
+class ColorAdmin(admin.ModelAdmin):
+    list_display = ["name", "code", "color_tag", ]
+
+
+admin.site.register(Size, SizeAdmin)
+admin.site.register(Color, ColorAdmin)
--- product/models.py
+++ product/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+from django.utils.safestring import mark_safe
+
+
+class Size(models.Model):
+    name = models.CharField(max_length=300)
+    code = models.CharField(max_length=10, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Material(models.Model):
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+
+class Color(models.Model):
+    name = models.CharField(max_length=300)
+    code = models.CharField(max_length=10, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def color_tag(self):
+        if self.code is not None:
+            return mark_safe('<p style ="background-color:{}">Color</p>'.format(self.code))
+        return ""
