9af871588a3496bd4884d2c3123eccb1a3e4f044
sahar <you@example.com>
2024-01-15 17:23:45 +0330

    "feat: add test for models of product app" _size, color,material and attribute models

--- product/tests/test_models.py
+++ product/tests/test_models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from model_bakery import baker
 
 from core.models import Image
-from product.models import Brand
+from product.models import Brand, Size, Material, Color, Attribute
 
 
 class TestBrandModel(TestCase):
@@ -22,3 +22,50 @@ class TestBrandModel(TestCase):
         result = self.brand.image_tag()
         self.assertIsNotNone(result)
         self.assertIn('<img src="', result)
+
+
+class TestSizeModel(TestCase):
+    def setUp(self):
+        self.size = Size.objects.create(name='Test Size', code='test-Size')
+
+    def test_model_str(self):
+        """Test __str__ method"""
+        self.assertEquals(str(self.size), 'Test Size')
+
+
+class TestMaterialModel(TestCase):
+    def setUp(self):
+        self.material = Material.objects.create(name='Test Material')
+
+    def test_model_str(self):
+        """Test __str__ method"""
+        self.assertEquals(str(self.material), 'Test Material')
+
+
+class TestColorModel(TestCase):
+    def setUp(self):
+        self.color_with_code = Color.objects.create(name="Test Color", code="#FF0000")
+        self.color_without_code = Color.objects.create(name="Test Color Without Code")
+
+    def test_model_str(self):
+        """Test __str__ method"""
+        self.assertEquals(str(self.color_with_code), 'Test Color')
+
+    def test_color_tag_with_code(self):
+        """test color_tag method return  color with a code  """
+        result = self.color_with_code.color_tag()
+        expected_part = 'style="background-color:#FF0000"'
+        actual_result = result.replace(" ", "")
+        self.assertIn(expected_part, actual_result)
+
+    def test_color_tag_without_code(self):
+        """Test the color_tag method for a color without a code"""
+        result = self.color_without_code.color_tag()
+        self.assertEqual(result, "")
+class TestAttributeModel(TestCase):
+    def setUp(self):
+        self.attribute = Attribute.objects.create(attribute='Test Attribute', type='test type')
+
+    def test_model_str(self):
+        """Test __str__ method"""
+        self.assertEquals(str(self.attribute), 'Test Attribute_test type')
