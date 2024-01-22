ce3238256d0027812202ae762e389e5483e94e2b
sahar <you@example.com>
2024-01-22 18:29:00 +0330

    feat: add classes to handle signup, login and logout by email 1. class RequestRegisterView to check who want to connect with our web site 2.so complete user register in class CompleteRegisterByEmailView 3.if user register before in web site, handle user login in class LoginRegisterVerifyCodeView

--- customers/views.py
+++ customers/views.py
@@ -1,5 +1,5 @@
 from django.contrib import messages
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, login
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.messages.views import SuccessMessageMixin
 from django.core.cache import cache
@@ -7,11 +7,11 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.utils.crypto import get_random_string
 from django.utils.encoding import force_bytes
-from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.utils.http import urlsafe_base64_encode
 from django.views import View
-from django.views.generic import TemplateView, CreateView, FormView
-from django.contrib.auth.views import PasswordResetView
-from utils import send_registration_email, send_otp_code_by_phone
+from django.views.generic import CreateView, FormView
+from django.contrib.auth import views as auth_view
+from utils import send_registration_email, send_otp_code_by_phone, generate_and_store_otp, send_otpcode_email
 from .forms import VerifyCodeFrom, RequestRegisterByEmailForm, \
     RequestRegistrationByPhoneFrom, UserCreationForm
 from .models import CustomUser
@@ -28,15 +28,32 @@ class RequestRegisterView(FormView):
     token_generator = default_token_generator
     uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
 
+    # def form_valid(self, form):
+    #     email = form.cleaned_data['email']
+    #     self.request.session['user_registration_info'] = {'email': email}
+    #     protocol = 'http' if not self.request.is_secure() else 'https'
+    #     domain = self.request.META['HTTP_HOST']
+    #
+    #     send_registration_email(email, self.uidb64, self.token_generator, protocol, domain)
+    #     messages.success(self.request, "The authentication confirmation link has been sent to your email", "success")
+    #     return super().form_valid(form)
+
     def form_valid(self, form):
         email = form.cleaned_data['email']
         self.request.session['user_registration_info'] = {'email': email}
-        protocol = 'http' if not self.request.is_secure() else 'https'
-        domain = self.request.META['HTTP_HOST']
-
-        send_registration_email(email, self.uidb64, self.token_generator, protocol, domain)
-        messages.success(self.request, "The authentication confirmation link has been sent to your email", "success")
-        return super().form_valid(form)
+        requester = user.objects.filter(email=email).exists()
+        if requester:
+            otp_code = generate_and_store_otp(email)
+            send_otpcode_email(email, otp_code, )
+            messages.success(self.request, "verification coded send for your email", "success")
+            return redirect('customers:verify_login_code')
+        else:
+            protocol = 'http' if not self.request.is_secure() else 'https'
+            domain = self.request.META['HTTP_HOST']
+            send_registration_email(email, self.uidb64, self.token_generator, protocol, domain)
+            messages.success(self.request, "The authentication confirmation link has been sent to your email",
+                             "success")
+            return super().form_valid(form)
 
 
 class CompleteRegisterByEmailView(SuccessMessageMixin, CreateView):
@@ -53,6 +70,36 @@ class CompleteRegisterByEmailView(SuccessMessageMixin, CreateView):
             self.object.email = user_email
             self.object.save()
         return response
+
+
+class LoginRegisterVerifyCodeView(View):
+    form_class = VerifyCodeFrom
+    template_name = 'customers/verify_code_toemail.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('core:home')
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        form = self.form_class
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        user_session = self.request.session.get('user_registration_info', {}).get('email')
+        code_instance = cache.get(user_session)
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            otp_code = form.cleaned_data['code']
+            if code_instance['otp_code'] == otp_code:
+                requester = user.objects.filter(email=user_session).first()
+                print("1"*50,requester)
+                login(request, requester, backend='django.contrib.auth.backends.ModelBackend')
+                print("2" * 50, requester)
+                return redirect('core:home')
+            else:
+                messages.error(request, 'this code is wrong', 'danger')
+                return redirect('accounts:verify_code')
 
 
 class RequestRegisterByPhoneView(View):
@@ -73,15 +120,17 @@ class RequestRegisterByPhoneView(View):
         if form.is_valid():
             phone = form.cleaned_data['phone']
             otp_code = self.generate_and_store_otp(phone)
+            print("3" * 60, "saraaaa")
             send_otp_code_by_phone(phone, otp_code)
+            print("4 " * 60, "saraaaa")
             request.session['user_registration_info'] = {'phone': phone}
             messages.success(request, "send registeration code to your Phone Number", "success")
             return redirect('customers:verify_registeration_code')
         return render(request, self.template_name, {'form': form})
 
     @staticmethod
-    def generate_and_store_otp(email):
-        cache_key = email
+    def generate_and_store_otp(phone):
+        cache_key = phone
         cached_data = cache.get(cache_key)
         if cached_data is not None:
             return cached_data['otp_code']
@@ -103,7 +152,7 @@ class CompleteRegisterVerifyCodeView(View):
 
     def get(self, request):
         form = self.form_class
-        return render(request, 'customers/verify_code.html', {'form': form})
+        return render(request, self.template_name, {'form': form})
 
     def post(self, request):
         user_session = request.session['user_registration_info']
@@ -111,13 +160,80 @@ class CompleteRegisterVerifyCodeView(View):
         # code_instance = cache.get(email=user_session['email'])
         form = self.form_class(request.POST)
         if form.is_valid():
+            print("7" * 60, "saraaaa")
             otp_code = form.cleaned_data['code']
             if code_instance['otp_code'] == otp_code:
-                CustomUser.objects.create_user(phone=user_session['phone'])
+                CustomUser.objects.create_user(phone=user_session['phone'], password="")
                 messages.success(request, 'you registered', 'success')
                 return redirect('core:home')
             else:
                 messages.error(request, 'this code is wrong', 'danger')
+
+
+# class CustomerRegisterView(View):
+#     form_class = UserRegistrationFrom
+#     template_name = 'customers/register.html'
+#
+#     def dispatch(self, request, *args, **kwargs):
+#         if request.user.is_authenticated:
+#             return redirect('home:home')
+#         return super().dispatch(request, *args, **kwargs)
+#
+#     def get(self, request):
+#         form = self.form_class
+#         return render(request, self.template_name, {'form': form})
+#
+#     def post(self, request):
+#         form = self.form_class(request.POST)
+#         if form.is_valid():
+#             email = form.cleaned_data['email_or_phone']
+#             otp_code = self.generate_and_store_otp(email)
+#             send_registration_email(email, otp_code)
+#             request.session['user_registration_info'] = {'email': email}
+#             messages.success(request, "send registeration code to your email", "success")
+#             return redirect('customers:verify_registeration_code')
+#         return render(request, self.template_name, {'form': form})
+#
+#     @staticmethod
+#     def generate_and_store_otp(email):
+#         cache_key = email
+#         cached_data = cache.get(cache_key)
+#         if cached_data is not None:
+#             return cached_data['otp_code']
+#         else:
+#             otp_code = get_random_string(length=6, allowed_chars="0123456789")
+#             data_to_cache = {"otp_code": otp_code}
+#             cache.set(cache_key, data_to_cache, timeout=60)
+#         return otp_code
+#
+#
+# class CustomerRegisterVerifyCodeView(View):
+#     form_class = VerifyCodeFrom
+#     template_name = 'customers/verify_code.html'
+#
+#     def dispatch(self, request, *args, **kwargs):
+#         if request.user.is_authenticated:
+#             return redirect('home:home')
+#         return super().dispatch(request, *args, **kwargs)
+#
+#     def get(self, request):
+#         form = self.form_class
+#         return render(request, 'customers/verify_code.html', {'form': form})
+#
+#     def post(self, request):
+#         user_session = request.session['user_registration_info']
+#         code_instance = cache.get(user_session['email'])
+#         # code_instance = cache.get(email=user_session['email'])
+#         form = self.form_class(request.POST)
+#         if form.is_valid():
+#             otp_code = form.cleaned_data['code']
+#             if code_instance['otp_code'] == otp_code:
+#                 CustomUser.objects.create_user(email=user_session['email'])
+#                 messages.success(request, 'you registered', 'success')
+#                 return redirect('home:home')
+#             else:
+#                 messages.error(request, 'this code is wrong', 'danger')
+#                 return redirect('accounts:verify_code')
 
 
 class CustomerloginCodeView(View):
@@ -128,6 +244,7 @@ class CustomerloginCodeView(View):
         pass
 
 
-class CustomerlogoutCodeView(View):
-    def get(self, request):
-        pass
+class UserLogoutView(auth_view.LogoutView):
+    class UserLogoutView(auth_view.LogoutView):
+        def get_success_url(self):
+            return self.request.GET.get('next', reverse_lazy('core:home'))
