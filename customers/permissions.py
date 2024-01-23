40094db5633f52434e05fc2e9c91b5da5f5ece9b
sahar <you@example.com>
2024-01-23 14:11:48 +0330

    feat: add methods to handle groups and permissions create custompermission methods so divided perssions which is definded in project in 3type 1. permission to all apps 2.permission to all models of special apps 3. add special permission for group

--- customers/permissions.py
+++ customers/permissions.py
@@ -1,42 +1,38 @@
-# from django.contrib.auth.models import Permission
-# from django.contrib.contenttypes.models import ContentType
-#
-#
-# class CustomPermission:
-#
-#     @staticmethod
-#     def create_one_type_permission_in_all_models(type_prem: str):
-#         codename = f'{type_prem}_all_models'
-#         name = f'Can {type_prem.capitalize()} All Models'
-#         print("3" * 40, name)
-#         permission, created = Permission.objects.get_or_create(codename=codename, name=name)
-#         print("3"*40,permission)
-#         return permission
-#
-#     @staticmethod
-#     def create_full_permission_for_app(app_name: str):
-#         print("5" * 40, ContentType.objects.all())
-#         content_type = ContentType.objects.get_for_models(app_name=app_name)
-#         print("4" * 40, content_type)
-#         permission_codename = f'can_access_{app_name}_app'
-#         permission_name = f'Can Access {app_name.capitalize()} App'
-#         print("3" * 40, permission_name)
-#         permission, created = Permission.objects.get_or_create(
-#             codename=permission_codename,
-#             name=permission_name,
-#             content_type=content_type,
-#         )
-#         print("3" * 40, permission)
-#         return permission
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
 
-    # @staticmethod
-    # def create_customer_management_permission():
-    #     content_type = ContentType.objects.get_for_model("CustomUser")
-    #     permission_codename = 'manage_customers'
-    #     permission_name = 'Can Add, Edit, and Delete Customers'
-    #     permission, created = Permission.objects.get_or_create(
-    #         codename=permission_codename,
-    #         name=permission_name,
-    #         content_type=content_type,
-    #     )
-    #     return permission
+list_group = ['managers', 'operators', 'supervisors', 'customers']
+
+
+def add_all_permission_of_app(app_label, group):
+    content_types = ContentType.objects.filter(app_label=app_label)
+    for content_type in content_types:
+        model_name = content_type.model
+        type_perms = ['add', 'change', 'delete', 'view']
+        for type_perm in type_perms:
+            permission_codename = f'{type_perm}_{model_name}'
+            permission = Permission.objects.get(content_type=content_type, codename=permission_codename)
+            group.permissions.add(permission)
+    return group
+
+
+def add_view_permission_to_group(group, type_perm):
+    content_types = ContentType.objects.all()
+    for content_type in content_types:
+        model_name = content_type.model
+        permission_codename = f'{type_perm}_{model_name.lower()}'
+        permission = Permission.objects.get(content_type=content_type, codename=permission_codename)
+        group.permissions.add(permission)
+    return group
+
+
+def add_full_permission(group):
+    content_types = ContentType.objects.all()
+    for content_type in content_types:
+        model_name = content_type.model
+        type_perms = ['add', 'change', 'delete', 'view']
+        for type_perm in type_perms:
+            permission_codename = f'{type_perm}_{model_name}'
+            permission = Permission.objects.get(content_type=content_type, codename=permission_codename)
+            group.permissions.add(permission)
+    return group
