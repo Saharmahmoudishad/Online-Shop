7ea674ed0ed8e6ee21ecf31cee2b7afb48f91de8
sahar <you@example.com>
2024-01-24 10:32:04 +0330

    refactor: modify AllProductView class inheriting from ListView add get_queryset method

--- product/views.py
+++ product/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import View
+from django.views.generic import ListView
 
 from product.models import Products
 
 
 
-class AllProductView(View):
-    def get(self, request):
-        products = Products.objects.all()
-        return render(request, 'product/shop.html', {'products': products})
+# class AllProductView(View):
+#     def get(self, request):
+#         products = Products.objects.all()
+#         return render(request, 'product/shop.html', {'products': products})
+class AllProductView(ListView):
+    model = Products
+    template_name = 'product/shop.html'
+    context_object_name = 'products'
+    paginate_by = 10  # Set the number of products to display per page
 
+    def get_queryset(self):
+        sort_option = self.request.GET.get('sort', 'latest')
+
+        if sort_option == 'latest':
+            return Products.objects.all().order_by('-created')
+        elif sort_option == 'expensive':
+            return Products.objects.all().order_by('-price')
+        elif sort_option == 'cheapest':
+            return Products.objects.all().order_by('price')
+        else:
+            return Products.objects.all().order_by('-created')
 
 class ProductDetailView(View):
     def get(self, request, slug):
