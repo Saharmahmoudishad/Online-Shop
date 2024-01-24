9a7b92467f9290696a3ae2ef654b9a12096d8c9c
sahar <you@example.com>
2024-01-24 18:48:35 +0330

    feat: add detailproduct class in views.py manage url for this view

--- product/urls.py
+++ product/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 app_name = 'product'
 urlpatterns = [
-     path('/', views.AllProductView.as_view(), name='products'),
+     path('', views.AllProductView.as_view(), name='products'),
+     path('product_detail/<slug:slug>', views.ProductDetailView.as_view(), name='product_detail'),
+
 ]
--- product/views.py
+++ product/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
-from product.models import Products
-
+from product.models import Products, Variants
 
 
 # class AllProductView(View):
@@ -17,21 +16,34 @@ class AllProductView(ListView):
     paginate_by = 10  # Set the number of products to display per page
 
     def get_queryset(self):
+        queryset = Products.objects.all()
+        # sorting logic
         sort_option = self.request.GET.get('sort', 'latest')
-
         if sort_option == 'latest':
-            return Products.objects.all().order_by('-created')
+            queryset = queryset.order_by('-created')
         elif sort_option == 'expensive':
-            return Products.objects.all().order_by('-price')
+            queryset = queryset.order_by('-price')
         elif sort_option == 'cheapest':
-            return Products.objects.all().order_by('price')
-        else:
-            return Products.objects.all().order_by('-created')
+            queryset = queryset.order_by('price')
 
-class ProductDetailView(View):
-    def get(self, request, slug):
-        product = get_object_or_404(Products, slug=slug)
-        return render(request, 'product/detail.html', {product: product})
+        price_range = self.request.GET.get('price', 'all')
+        if price_range != 'all':
+            # Extract min and max values from the price_range
+            min_price, max_price = map(int, price_range.split('-'))
+            queryset = queryset.filter(price__gte=min_price, price__lte=max_price)
 
-    def post(self, request):
-        pass
+        return queryset
+
+
+
+class ProductDetailView(DetailView):
+    model = Products
+    template_name = 'product/detail.html'
+    context_object_name = 'product'
+    slug_url_kwarg = 'slug'  # Use slug_url_kwarg instead of pk_url_kwarg
+
+    def get_object(self, queryset=None):
+        # Override get_object to use slug for lookup
+        slug = self.kwargs.get(self.slug_url_kwarg)
+        return get_object_or_404(self.model, slug=slug)
+
