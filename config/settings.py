e6b4e0e2156f8ee8b6e91073cf5769cfd6e7603b
sahar <you@example.com>
2024-01-26 00:56:21 +0330

    refactor: modify theme of admin panel

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,8 @@ ALLOWED_HOSTS = ['*']
 # -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 # Application definition
 INSTALLED_APPS = [
+    "admin_interface",
+    "colorfield",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
