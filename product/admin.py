bcfd1a7bd6123907ae9df0474bc4cdf6085cd6aa
sahar <you@example.com>
2024-01-28 11:13:46 +0330

    refactor:modify CategoryProductAdmin class 1.add get_queryset method 2.add related_products_count method 3.add related_products_cumulative_count method

--- product/admin.py
+++ product/admin.py
@@ -1,12 +1,46 @@
 from django.contrib import admin
+from mptt.admin import DraggableMPTTAdmin
 
 from product.models import Color, Size, CategoryProduct, Brand, Material, Attribute, Products, Variants
 
 
-class CategoryProductAdmin(admin.ModelAdmin):
-    list_display = ["id", "title", "parent", "status", "image_tag", ]
-    search_field = ["title", ]
-    list_filter = ["parent", ]
+# class CategoryProductAdmin(admin.ModelAdmin):
+#     list_display = ["id", "title", "parent", "status", "image_tag", ]
+#     search_field = ["title", ]
+#     list_filter = ["parent", ]
+
+class CategoryProductAdmin(DraggableMPTTAdmin):
+    mptt_indent_field = "title"
+    list_display = ('tree_actions', 'indented_title',
+                    'related_products_count', 'related_products_cumulative_count')
+    list_display_links = ('indented_title',)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+
+        # Add cumulative product count
+        qs = CategoryProduct.objects.add_related_count(
+                qs,
+                Products,
+                'category',
+                'products_cumulative_count',
+                cumulative=True)
+
+        # Add non cumulative product count
+        qs = CategoryProduct.objects.add_related_count(qs,
+                 Products,
+                 'category',
+                 'products_count',
+                 cumulative=False)
+        return qs
+
+    def related_products_count(self, instance):
+        return instance.products_count
+    related_products_count.short_description = 'Related products (for this specific category)'
+
+    def related_products_cumulative_count(self, instance):
+        return instance.products_cumulative_count
+    related_products_cumulative_count.short_description = 'Related products (in tree)'
 
 
 class BrandAdmin(admin.ModelAdmin):
