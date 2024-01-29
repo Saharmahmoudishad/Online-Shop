51673771a4c0e865a1b07e33f5bf1f15287a64d2
sahar <you@example.com>
2024-01-29 16:19:47 +0330

    feat: add urls for allProductViews for Categories_filter"

--- product/urls.py
+++ product/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'product'
 urlpatterns = [
      path('', views.AllProductView.as_view(), name='products'),
+     path('category/<slug:slug>', views.AllProductView.as_view(), name='category_filter'),
      path('product_detail/<slug:slug>', views.ProductDetailView.as_view(), name='product_detail'),
 
 ]
