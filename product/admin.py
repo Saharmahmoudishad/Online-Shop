3e7ab58b987111dec801c0c9b74531705568c12a
sahar <you@example.com>
2024-01-29 20:02:19 +0330

    refactor: modify admin panel by customing in django inline admin categories inline add in products models products inline add in variants models

--- product/admin.py
+++ product/admin.py
@@ -1,84 +1,105 @@
 from django.contrib import admin
 from mptt.admin import DraggableMPTTAdmin
 
-from product.models import Color, Size, CategoryProduct, Brand, Material, Attribute, Products, Variants
+from product.models import Color, Size, CategoryProduct, Brand, Material, Attribute, Products, Variants, \
+    DiscountProduct, Image
+
+
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 1
+
+
+class ProductInline(admin.TabularInline):
+    model = Products
+    readonly_fields = ('id',)
+    extra = 1
+
 
 class CategoryProductAdmin(DraggableMPTTAdmin):
     mptt_indent_field = "title"
     list_display = ('tree_actions', 'indented_title',
                     'related_products_count', 'related_products_cumulative_count', "status", "image_tag",)
     list_display_links = ('indented_title',)
-    # draggable = True
+    inlines = [ProductInline]
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
 
-        # Add cumulative product count
         qs = CategoryProduct.objects.add_related_count(
-                qs,
-                Products,
-                'category',
-                'products_cumulative_count',
-                cumulative=True)
+            qs,
+            Products,
+            'category',
+            'products_cumulative_count',
+            cumulative=True)
 
-        # Add non cumulative product count
         qs = CategoryProduct.objects.add_related_count(qs,
-                 Products,
-                 'category',
-                 'products_count',
-                 cumulative=False)
+                                                       Products,
+                                                       'category',
+                                                       'products_count',
+                                                       cumulative=False)
         return qs
 
     def related_products_count(self, instance):
         return instance.products_count
+
     related_products_count.short_description = 'Related products (for this specific category)'
 
     def related_products_cumulative_count(self, instance):
         return instance.products_cumulative_count
+
     related_products_cumulative_count.short_description = 'Related products (in tree)'
 
 
 class BrandAdmin(admin.ModelAdmin):
     list_display = ["title", "image_tag", ]
-    search_field = ["title", ]
+    search_fields = ["title", ]
 
 
 class SizeAdmin(admin.ModelAdmin):
     list_display = ["gender", "code", ]
-    search_field = ["code", ]
+    search_fields = ["code", ]
     list_filter = ["gender", ]
 
 
 class ColorAdmin(admin.ModelAdmin):
     list_display = ["name", "code", "color_tag", ]
-    search_field = ["name", "code", ]
+    search_fields = ["name", "code", ]
 
 
 class MaterialAdmin(admin.ModelAdmin):
     list_display = ["name", ]
-    search_field = ["name", ]
+    search_fields = ["name", ]
 
 
 class AttributeAdmin(admin.ModelAdmin):
     list_display = ["attribute", "type", ]
-    search_field = ["attribute", ]
+    search_fields = ["attribute", ]
+
+
+class VariantsInline(admin.TabularInline):
+    model = Variants
+    readonly_fields = ('id',)
+    extra = 1
+
 
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ["title", "category", "status", "image_tag", ]
-    search_field = ["category", "title", ]
+    search_fields = ["category__title", "title", ]
     list_filter = ["category", "quantity"]
+    inlines = [VariantsInline,]
 
 
 class VariantsAdmin(admin.ModelAdmin):
-    list_display = ["title", "color", "size", "brand", "material", "attribute", "price", "quantity",]
-    search_field = ["product", ]
+    list_display = ["title", "color", "size", "brand", "material", "attribute", "price", "quantity", ]
+    search_fields = ["product__title", 'brand__name']
     list_filter = ["product", "title", "quantity"]
 
 
 class DiscountProductAdmin(admin.ModelAdmin):
-    list_display = ["title", "product", "time",]
-    list_filter = ["title",]
+    list_display = ["title", "product", "deadline", "amount", ]
+    list_filter = ["title", ]
 
 
 admin.site.register(CategoryProduct, CategoryProductAdmin)
@@ -89,3 +110,4 @@ admin.site.register(Material, MaterialAdmin)
 admin.site.register(Attribute, AttributeAdmin)
 admin.site.register(Products, ProductAdmin)
 admin.site.register(Variants, VariantsAdmin)
+admin.site.register(DiscountProduct, DiscountProductAdmin)
