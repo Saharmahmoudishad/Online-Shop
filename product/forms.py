b3cca25bd4d46015f0244e3edd197847ebda890e
sahar <you@example.com>
2024-01-31 09:11:58 +0330

    feat: add replycommentform class

--- product/forms.py
+++ product/forms.py
@@ -6,6 +6,19 @@ class CommentToManagerForm(forms.ModelForm):
     """
     Class for handel the user comment to Manager.
     """
+
     class Meta:
         model = Comment
         fields = ('content',)
+        widgets = {'body': forms.Textarea(attrs={'class': 'form-control'})}
+
+
+class CommentReplyForm(forms.ModelForm):
+    """
+    Class for handel the user comment to Manager.
+    """
+
+    class Meta:
+        model = Comment
+        fields = ('content',)
+        widget = forms.Textarea(attrs={'class': 'form-control'})
