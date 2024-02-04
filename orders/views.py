359779925bb601efc8db9a84552b87cb72f19da9
sahar <you@example.com>
2024-02-04 19:15:32 +0330

    feat: create all necessary class for cart and checkout pages 1. CartAddView class 2. CartRemoveView class 3. OrderDetailView class 4. OrderCreateView class

--- orders/views.py
+++ orders/views.py
@@ -1,34 +1,52 @@
 from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
+from orders.cart import Cart
+from orders.forms import CartAddForm
+from orders.models import Order, OrderItem
+from product.models import Products
 
 
 # Create your views here.
 class CartView(View):
     def get(self, request):
-        pass
+        cart = Cart(request)
+        return render(request, 'orders/cart.html', {'cart': cart})
 
-    def post(self, request):
-        pass
+
 
 
 class CartAddView(PermissionRequiredMixin, View):
 
-    def post(self, request):
-        pass
+    def post(self, request, product_id):
+        cart = Cart(request)
+        product = get_object_or_404(Products, id=product_id)
+        form = CartAddForm(request.POST)
+        if form.is_valid():
+            cart.add(product, form.cleaned_data['quantity'])
+            return redirect('orders:cart')
 
 
 class CartRemoveView(View):
-    def get(self, request):
-        pass
+    def get(self, request, product_id):
+        cart = Cart(request)
+        product = get_object_or_404(Products, id=product_id)
+        cart.remove(product)
+        return redirect('orders:cart')
 
 
 class OrderDetailView(LoginRequiredMixin, View):
-    def get(self, request):
-        pass
+    def get(self, request, order_id):
+        order = get_object_or_404(Order, id=order_id)
+        return render(request, 'orders/order.html', {'order': order})
 
 
 class OrderCreateView(LoginRequiredMixin, View):
 
     def get(self, request):
-        pass
+        cart = Cart(request)
+        order = Order.objects.create(user=request.user)
+        for item in cart:
+            OrderItem.objects.create(order=order, items=item['product'], quantity=item['quanlity'])
+        cart.clear()
+        return redirect('orders:oder_detail', order.id)
