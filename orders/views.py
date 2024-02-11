4955865e53e9a80ba1e66b179f0aa7161405d171
sahar <you@example.com>
2024-02-11 15:22:23 +0330

    refactor: add put method to CartViewcalass for handle update cart

--- orders/views.py
+++ orders/views.py
@@ -1,79 +1,20 @@
-from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
-from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render
 from django.views import View
 from rest_framework import status
-from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from orders.cart import Cart
-from orders.forms import CartAddForm
 from orders.models import Order, OrderItem
+from orders.API.serializers import OrderSerializer, OrderItemSerializer
 from product.models import Products, Variants, Size, Brand, Color, Material, Attribute
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
-class CartView(APIView):
-    renderer_classes = [TemplateHTMLRenderer]
+class CartView(View):
     template_name = 'orders/cart.html'
 
     def get(self, request):
         cart = Cart(request)
-
-        return Response(
-            {"cart": cart, "data": list(cart.__iter__()),
-             "cart_total_price": cart.get_total_price()},
-            status=status.HTTP_200_OK
-        )
-
-    def post(self, request, product_id=None, **kwargs):
-        cart = Cart(request)
-        product = get_object_or_404(Products, id=product_id)
-
-        if "remove" in request.data:
-            variant = request.data["variant"]
-            cart.remove(variant)
-
-        elif "clear" in request.data:
-            cart.clear()
-
-        else:
-            variant = request.data
-
-            post_data = {key: int(value[0:20]) if value.isdigit() else value[0:20] for key, value in
-                         request.data.items()}
-            post_data['size'] = Size.objects.filter(code=post_data['size']).first()
-            post_data['brand'] = Brand.objects.filter(title=post_data['brand']).first()
-            post_data['color'] = Color.objects.filter(name=post_data['color']).first()
-            post_data['material'] = Material.objects.filter(name=post_data['material']).first()
-            post_data['attribute'] = Attribute.objects.filter(type=post_data['attribute']).first()
-            variant_chose = get_object_or_404(Variants, product=product_id,
-                                              brand=post_data['brand'],
-                                              size=post_data['size'], color=post_data['color'],
-                                              material=post_data['material'],
-                                              attribute=post_data['attribute'], )
-
-            cart.add(variant=variant_chose,
-                     discount_price=post_data['discount'],
-                     quantity=post_data['quantity'],
-                     brand=post_data['brand'].title,
-                     color=post_data['color'].name,
-                     size=post_data['size'].code,
-                     material=post_data['material'].name,
-                     attribute=post_data['attribute'].type, )
-            product = get_object_or_404(Products, id=product_id)
-
-        return Response(
-            {"message": "cart updated", },
-            status=status.HTTP_202_ACCEPTED)
-
-
-class CartRemoveView(APIView):
-    def get(self, request, variant_id):
-        print("1"*60,"aaa")
-        cart = Cart(request)
-        variant = get_object_or_404(Variants, id=variant_id)
-        cart.remove(variant)
-        print("2" * 60,"www")
-        return Response('orders:cart')
-
-
-
+        return render(request, self.template_name, {"cart": cart, "data": list(cart.__iter__()),
+                                                    "cart_total_price": cart.get_total_price()}, )
