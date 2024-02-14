e5dfd66746d8b3b66b1486ae345e7339cb8606fb
sahar <you@example.com>
2024-02-14 10:23:22 +0330

    refactor: add serializers relations for models of orders apps

--- orders/API/serializers.py
+++ orders/API/serializers.py
@@ -4,14 +4,20 @@ from orders.models import OrderItem, Order
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
+    items =serializers.StringRelatedField(read_only=True)
     class Meta:
         model = OrderItem
         fields = ['quantity', 'items']
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    orderItem = OrderItemSerializer(many=True, read_only=True)
+    orderitems = serializers.SerializerMethodField()
+    user = serializers.StringRelatedField(read_only=True)
 
     class Meta:
         model = Order
-        fields = ['id', 'user', 'paid', 'order_time', 'orderItem', 'delivery_cost', 'calculation']
+        fields = ['id', 'user', 'paid', 'order_time', 'delivery_cost', 'calculation',"orderitems"]
+
+    def get_orderitems(self, obj):
+        customer_items = obj.orderItem.all()
+        return OrderItemSerializer(instance=customer_items, many=True).data
